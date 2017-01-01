b4bca47776919be83911bea6a463c61719813b04
hyeonil <dev.nuti0102@gmail.com>
2017-01-02 08:56:25 +0900

    set sonar

---
+++ review/gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function (grunt) {
+
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-saucelabs');
+
+  grunt.registerTask('default', ['connect', 'saucelabs-mocha']);
+};
